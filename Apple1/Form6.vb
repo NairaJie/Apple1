d0f3a384bf8f9845715cf03c822db788d477fd5e
Nailah Mahira <68544297+NairaJie@users.noreply.github.com>
2025-01-15 01:43:07 +0700

    Update Form6.vb
    added new msgbox for stock

--- Apple1/Form6.vb
+++ Apple1/Form6.vb
@@ -1,18 +1,11 @@
-﻿Imports System.Net.Mime.MediaTypeNames
+Imports System.Net.Mime.MediaTypeNames
+Imports MySql.Data.MySqlClient
 
 Public Class FormDetailTrans
     Dim totbay As Double
 
     Private Sub FormDetailTrans_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        kon.Open()
-        perintah.Connection = kon
-        perintah.CommandType = CommandType.Text
-        perintah.CommandText = "select * from mauw"
-        mda.SelectCommand = perintah
-        ds.Tables.Clear()
-        mda.Fill(ds, "mauw")
-        dgvdetail.DataSource = ds.Tables("mauw")
-        kon.Close()
+        tampildata()
     End Sub
 
     Sub tampildata()
@@ -28,11 +21,12 @@ Public Class FormDetailTrans
     End Sub
 
     Private Sub btnsimpan_Click(sender As Object, e As EventArgs) Handles btnsimpan.Click
+        If kon.State = ConnectionState.Open Then kon.Close()
         kon.Open()
         perintah.Connection = kon
         perintah.CommandType = CommandType.Text
         perintah.CommandText = "insert into detailtransaksi values('" & txtidtransaksi.Text & "','" & txtidproduk.Text & "','" & txtjumlah.Text & "', " &
-        " '" & txttotal.Text & "', '" & txtstatus.Text & "', '" & txtmetode.Text & "')"
+        " '" & txttotal.Text & "', '" & txtstatus.Text & "', '" & cmbmetode.Text & "')"
         perintah.ExecuteNonQuery()
         kon.Close()
         tampildata()
@@ -55,22 +49,23 @@ Public Class FormDetailTrans
                 txtnamastaff.Text = .Cells(5).Value
                 txtidproduk.Text = .Cells(6).Value
                 txtnamaproduk.Text = .Cells(7).Value
-                txtharga.Text = .Cells(8).Value
-                txtjumlah.Text = .Cells(9).Value
-                txttotal.Text = .Cells(10).Value
-                txtstatus.Text = .Cells(11).Value
-                txtmetode.Text = .Cells(12).Value
+                txtharga.Text = .Cells(9).Value
+                txtjumlah.Text = .Cells(10).Value
+                txttotal.Text = .Cells(11).Value
+                txtstatus.Text = .Cells(12).Value
+                cmbmetode.Text = .Cells(13).Value
             End With
         Catch ex As Exception
         End Try
     End Sub
 
     Private Sub btnupdate_Click(sender As Object, e As EventArgs) Handles btnupdate.Click
+        If kon.State = ConnectionState.Open Then kon.Close()
         kon.Open()
         perintah.Connection = kon
         perintah.CommandType = CommandType.Text
         perintah.CommandText = "UPDATE detailtransaksi SET idproduk='" &
-        txtidproduk.Text & "', jumlah='" & txtjumlah.Text & "', totalbayar ='" & txttotal.Text & "', statuspembayaran ='" & txtstatus.Text & "', metodepembayaran='" & txtmetode.Text & "'  WHERE idtransaksi= '" &
+        txtidproduk.Text & "', jumlah='" & txtjumlah.Text & "', totalbayar ='" & txttotal.Text & "', statuspembayaran ='" & txtstatus.Text & "', metodepembayaran='" & cmbmetode.Text & "'  WHERE idtransaksi= '" &
         txtidtransaksi.Text & "'"
         perintah.ExecuteNonQuery()
         kon.Close()
@@ -79,6 +74,7 @@ Public Class FormDetailTrans
     End Sub
 
     Private Sub btnhapus_Click(sender As Object, e As EventArgs) Handles btnhapus.Click
+        If kon.State = ConnectionState.Open Then kon.Close()
         kon.Open()
         perintah.Connection = kon
         perintah.CommandType = CommandType.Text
@@ -102,7 +98,23 @@ Public Class FormDetailTrans
     End Sub
 
     Private Sub txtjumlah_TextChanged(sender As Object, e As EventArgs) Handles txtjumlah.TextChanged
+        If kon.State = ConnectionState.Open Then kon.Close()
+        kon.Open()
+        perintah.Connection = kon
+        perintah.CommandType = CommandType.Text
+        perintah.CommandText = "select * from produk"
+        cek = perintah.ExecuteReader()
+        cek.Read()
+        If cek.HasRows Then
+            Dim stok As Integer = cek.Item("stok")
+            If Val(txtjumlah.Text) > stok Then
+                MsgBox("Pesanan Anda melebihi stok yang tersedia", MsgBoxStyle.Information, "Pesan")
+            End If
+        End If
+
+        kon.Close()
         totbay = Val(txtharga.Text) * Val(txtjumlah.Text)
         txttotal.Text = totbay
     End Sub
+
 End Class
